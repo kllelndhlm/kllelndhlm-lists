62f43ac16722abb6c2cd695bd0cfc6c2d0080efa
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-04-25 23:31:33 +0300

    Database

--- db.py
+++ db.py
@@ -5,4 +5,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
