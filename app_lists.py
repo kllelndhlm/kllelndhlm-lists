c9772d29ec91095602e254508031f2cf4f256ad8
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-05-08 22:34:47 +0300

    Kooste SQL

--- app_lists.py
+++ app_lists.py
@@ -40,13 +40,13 @@ def statistics():
     sql = "SELECT artist, COUNT(artist) AS artist_occurence FROM list WHERE visible=1 GROUP BY artist ORDER BY artist_occurence DESC LIMIT 3"
     result = db.session.execute(sql)
     most_frequent_artist = result.fetchall()
-    sql = "SELECT artist, song, COUNT(song) AS song_occurence FROM list GROUP BY artist, song ORDER BY song_occurence DESC LIMIT 3"
+    sql = "SELECT artist, song, COUNT(song) AS song_occurence FROM list WHERE visible=1 GROUP BY artist, song ORDER BY song_occurence DESC LIMIT 3"
     result = db.session.execute(sql)
     most_frequent_song = result.fetchall()
-    sql = "SELECT genre, COUNT(genre) AS genre_occurence FROM list GROUP BY genre ORDER BY genre_occurence DESC LIMIT 3"
+    sql = "SELECT genre, COUNT(genre) AS genre_occurence FROM list WHERE visible=1 GROUP BY genre ORDER BY genre_occurence DESC LIMIT 3"
     result = db.session.execute(sql)
     most_frequent_genre = result.fetchall()
-    sql = "SELECT year, COUNT(year) AS year_occurence FROM list GROUP BY year ORDER BY year_occurence DESC LIMIT 3"
+    sql = "SELECT year, COUNT(year) AS year_occurence FROM list WHERE visible=1 GROUP BY year ORDER BY year_occurence DESC LIMIT 3"
     result = db.session.execute(sql)
     most_frequent_year = result.fetchall()
     return render_template("statistics.html", distinct_list = distinct_list, count=count, lists=lists, most_frequent_artist = most_frequent_artist, most_frequent_song = most_frequent_song,  most_frequent_genre =  most_frequent_genre, most_frequent_year = most_frequent_year)
